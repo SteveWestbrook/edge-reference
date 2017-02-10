cdd8d45684ee1b38e4c7cfad79046f2cf7bc7e31
Steve Westbrook <StevenAWestbrook@gmail.com>
2017-02-10 09:36:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * index of is-valid-var-name module
+ * Copyright(c) 2017 Steve Westbrook
+ * MIT Licensed
+ */
+
+'use strict';
+
+const EdgeReference = require('./src/edge-reference.js');
+
+module.exports = EdgeReference;
---
+++ src/edge-reference.js
@@ -0,0 +1,15 @@
+/**
+ * index of is-valid-var-name module
+ * Copyright(c) 2017 Steve Westbrook
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = EdgeReference;
+
+class EdgeReference {
+  constructor(assemblyInfo) {
+    
+  }
+}
